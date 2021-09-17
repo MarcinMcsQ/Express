d528becd96d735e25fbd90e5683d554534958ee8
Marcin <marcinlacala@gmail.com>
2021-09-17 14:38:10 +0200

    last version

--- models/quiz.js
+++ models/quiz.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const quizSchema = new Schema({
   title: { type: String, required: true }, // require tru sprawia ze parametr jest wymagany przy zapsisie w bazie danych
-  vote: { type: Number, required: true },
+  vote: { type: Number, required: true, default: 0 },
 });
 
 module.exports = mongoose.model("Quiz", quizSchema);
